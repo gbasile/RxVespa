2af7de057ce748a60515bf981a7d4b0f1fa33ee6
Giuseppe Basile <giuseppe@novoda.com>
2016-05-02 16:55:57 +0200

    Initial Commit

---
+++ VespaGASTests/VespaGASTests.swift
@@ -0,0 +1,36 @@
+//
+//  VespaGASTests.swift
+//  VespaGASTests
+//
+//  Created by Giuseppe Basile on 02/05/2016.
+//  Copyright © 2016 Giuseppe Basile. All rights reserved.
+//
+
+import XCTest
+@testable import VespaGAS
+
+class VespaGASTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
