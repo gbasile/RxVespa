83dacd623b5b7ca99de050e262699bb6f6cad18b
Giuseppe Basile <giuseppe@novoda.com>
2016-06-02 20:18:15 +0200

    Asynchronous testing

--- VespaGASTests/ViewModels/GasCalculatorViewModelTests.swift
+++ VespaGASTests/ViewModels/GasCalculatorViewModelTests.swift
@@ -1,67 +1,74 @@
 import XCTest
 import RxSwift
+import RxTests
+
 @testable import VespaGAS
 
 class GasCalculatorViewModelTests: XCTestCase {
     let disposeBag = DisposeBag()
 
+    var scheduler: TestScheduler!
+    var testableObserver: TestableObserver<String>!
+
+    override func setUp() {
+        super.setUp()
+
+        scheduler = TestScheduler(initialClock: 0)
+        testableObserver = scheduler.createObserver(String)
+    }
+
     //MARK: oilMixObservable
     func testOilMixValueObservableIsInitialisedWith0() {
-        let expectation = self.expectationWithDescription("Oil Observable should start with 0ml")
-
         let viewModel = GasCalculatorViewModel(
             gasObservable: Observable.empty(),
             oilMixObservable: Observable.empty()
         )
 
-        _ = viewModel.oilMixValueObservable.subscribeNext { (oilML) in
-            XCTAssertEqual("0ml", oilML)
-            expectation.fulfill()
-            }.addDisposableTo(disposeBag)
+        viewModel.oilMixValueObservable.subscribe(testableObserver)
+        scheduler.start()
 
-        self.waitForExpectationsWithTimeout(0.1, handler: nil)
+        let expectedEvents = [
+            next(0, "0ml"),
+            completed(0)
+        ]
+
+        XCTAssertEqual(expectedEvents, testableObserver.events)
     }
 
     func testOilMixValueObservableIsCorrectWhen1LiterAnd1OilPercentage() {
-        let expectation = self.expectationWithDescription("Oil Observable should be 10")
-
         let viewModel = GasCalculatorViewModel(
             gasObservable: Observable.just(Gasoline(amount: 1, unit: .Liter)),
             oilMixObservable: Observable.just(OilMix(amount: 1))
         )
 
-        var isFirstEvent = true
-        _ = viewModel.oilMixValueObservable.subscribeNext { (oilML) in
-            if isFirstEvent {
-                isFirstEvent = false
-            } else {
-                XCTAssertEqual("10ml", oilML)
-                expectation.fulfill()
-            }
-            }.addDisposableTo(disposeBag)
+        viewModel.oilMixValueObservable.subscribe(testableObserver)
+        scheduler.start()
 
-        self.waitForExpectationsWithTimeout(0.1, handler: nil)
+        let expectedEvents = [
+            next(0, "0ml"),
+            next(0, "10ml"),
+            completed(0)
+        ]
+
+        XCTAssertEqual(expectedEvents, testableObserver.events)
     }
 
     func testOilMixValueObservableRoundResult() {
-        let expectation = self.expectationWithDescription("Oil Observable should be 13")
-
         let viewModel = GasCalculatorViewModel(
             gasObservable: Observable.just(Gasoline(amount: 1.23, unit: .Liter)),
             oilMixObservable: Observable.just(OilMix(amount: 1.5))
         )
 
-        var isFirstEvent = true
-        _ = viewModel.oilMixValueObservable.subscribeNext { (oilML) in
-            if isFirstEvent {
-                isFirstEvent = false
-            } else {
-                XCTAssertEqual("18ml", oilML)
-                expectation.fulfill()
-            }
-            }.addDisposableTo(disposeBag)
+        viewModel.oilMixValueObservable.subscribe(testableObserver)
+        scheduler.start()
 
-        self.waitForExpectationsWithTimeout(0.1, handler: nil)
+        let expectedEvents = [
+            next(0, "0ml"),
+            next(0, "18ml"),
+            completed(0)
+        ]
+
+        XCTAssertEqual(expectedEvents, testableObserver.events)
     }
 
     //MARK: OilValueObservable
@@ -75,40 +82,38 @@ class GasCalculatorViewModelTests: XCTestCase {
     }
 
     func testOilValueObservableIsInitialisedWith0() {
-        let expectation = self.expectationWithDescription("Oil Value should start with 0%")
-
         let viewModel = GasCalculatorViewModel(
             gasObservable: Observable.empty(),
             oilMixObservable: Observable.empty()
         )
 
-        _ = viewModel.oilValueObservable.subscribeNext { (oilValue) in
-                XCTAssertEqual("0%", oilValue)
-                expectation.fulfill()
-            }.addDisposableTo(disposeBag)
+        viewModel.oilValueObservable.subscribe(testableObserver)
+        scheduler.start()
 
-        self.waitForExpectationsWithTimeout(0.1, handler: nil)
+        let expectedEvents = [
+            next(0, "0%"),
+            completed(0)
+        ]
+
+        XCTAssertEqual(expectedEvents, testableObserver.events)
     }
 
     func testOilValueObservableIsCorrectWhen2Percentage() {
-        let expectation = self.expectationWithDescription("Oil Value should be 2%")
-
         let viewModel = GasCalculatorViewModel(
             gasObservable: Observable.empty(),
             oilMixObservable: Observable.just(OilMix(amount: 2))
         )
 
-        var isFirstEvent = true
-        _ = viewModel.oilValueObservable.subscribeNext { oilValue in
-                if(isFirstEvent) {
-                    isFirstEvent = false
-                    return
-                }
-                XCTAssertEqual("2%", oilValue)
-                expectation.fulfill()
-            }.addDisposableTo(disposeBag)
+        viewModel.oilValueObservable.subscribe(testableObserver)
+        scheduler.start()
 
-        self.waitForExpectationsWithTimeout(0.1, handler: nil)
+        let expectedEvents = [
+            next(0, "0%"),
+            next(0, "2%"),
+            completed(0)
+        ]
+
+        XCTAssertEqual(expectedEvents, testableObserver.events)
     }
 
     //MARK: GasValueObservable
@@ -122,43 +127,37 @@ class GasCalculatorViewModelTests: XCTestCase {
     }
 
     func testGasVaueObservableIsInitalisedWith0() {
-        let expectation = self.expectationWithDescription("Gas Value should start with 0L")
-
         let viewModel = GasCalculatorViewModel(
             gasObservable: Observable.empty(),
             oilMixObservable: Observable.empty()
         )
 
-        var isFirstEvent = true
-        _ = viewModel.gasValueObservable.subscribeNext { (oilValue) in
-            if (isFirstEvent) {
-                XCTAssertEqual("0L", oilValue)
-                expectation.fulfill()
-                isFirstEvent = false
-            }
-            }.addDisposableTo(disposeBag)
+        viewModel.gasValueObservable.subscribe(testableObserver)
+        scheduler.start()
 
-        self.waitForExpectationsWithTimeout(0.1, handler: nil)
+        let expectedEvents = [
+            next(0, "0L"),
+            completed(0)
+        ]
+
+        XCTAssertEqual(expectedEvents, testableObserver.events)
     }
 
     func testGasValueObservableIsCorrectWhen3Liters() {
-        let expectation = self.expectationWithDescription("Gas Value should be 3L")
-
         let viewModel = GasCalculatorViewModel(
             gasObservable: Observable.just(Gasoline(amount: 3, unit: .Liter)),
             oilMixObservable: Observable.empty()
         )
 
-        var isFirstEvent = true
-        _ = viewModel.gasValueObservable.subscribeNext { oilValue in
-            if (isFirstEvent) {
-                isFirstEvent = false
-                return
-            }
-            XCTAssertEqual("3L", oilValue)
-            expectation.fulfill()
-            }.addDisposableTo(disposeBag)
+        viewModel.gasValueObservable.subscribe(testableObserver)
+        scheduler.start()
 
-        self.waitForExpectationsWithTimeout(0.1, handler: nil)
+        let expectedEvents = [
+            next(0, "0L"),
+            next(0, "3L"),
+            completed(0)
+        ]
+
+        XCTAssertEqual(expectedEvents, testableObserver.events)
     }
 }
