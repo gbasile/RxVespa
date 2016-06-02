6dee187408985a64cfb0a7e5a6b9ccda730ec918
Giuseppe Basile <giuseppe@novoda.com>
2016-06-02 20:17:49 +0200

    Updated Pod for RXTests

---
+++ Pods/Target Support Files/Pods-VespaGAS-VespaGASTests/Pods-VespaGAS-VespaGASTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VespaGAS_VespaGASTests : NSObject
+@end
+@implementation PodsDummy_Pods_VespaGAS_VespaGASTests
+@end
---
+++ Pods/Target Support Files/Pods-VespaGAS-VespaGASTests/Pods-VespaGAS-VespaGASTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_VespaGAS_VespaGASTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_VespaGAS_VespaGASTestsVersionString[];
+
